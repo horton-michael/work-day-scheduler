8f455089b45406a3dd935f402d09c9125506ebf0
mhorton <horton-michael@users.noreply.github.com>
2023-08-31 20:31:16 -0400

    adds event listener to button elements and stores the user input with the corresponding hour in local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,13 +4,12 @@
 // DOM ELEMENTS ---------------------------------------------------------------
 var currentDayEl = $("#currentDay");
 var timeBlocksEl = $(".time-block");
-
+var saveBtnEl = $(".saveBtn");
 // DATA ----------------------------------------------------------------------
 var today = dayjs().format("dddd, MMMM D");
 var currentTime = dayjs().hour();
 console.log(currentTime);
 // FUNCTIONS -----------------------------------------------------------------
-
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -18,7 +17,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
+  saveBtnEl.on("click", function (event) {
+    event.preventDefault();
+    // get the id of the time block that the save button was clicked in
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+    // get the user input
+    var userInput = $(this).siblings(".description").val();
+    // save the hour and user input in local storage
+    localStorage.setItem(timeBlockId, userInput);
+  });
   // create conditional statement within for loop to determine if the time is past, present, or future
   for (var i = 9; i <= 17; i++) {
     var timeBlockEl = $("#hour-" + i);
